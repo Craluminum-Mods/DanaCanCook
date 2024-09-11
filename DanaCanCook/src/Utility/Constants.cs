181790d8a6549c89d3ded00307db54e195d87410
Dana <beware.14b@gmail.com>
2024-09-11 17:24:41 +0300

    minor tweaks

--- DanaCanCook/src/Utility/Constants.cs
+++ DanaCanCook/src/Utility/Constants.cs
@@ -17,8 +17,10 @@ public static class Constants
     public const string langEmpty = "Empty";
     public const string langContents0x1 = "Contents: {0}x {1}";
     public const string langContents = "Contents: ";
+    public const string langNutritionfacts = "nutrition-facts-line-satiety";
     public const string langSandwichContents = $"{ModId}:contents-sandwich";
     public const string langSandwich = $"{ModId}:item-sandwich";
+    public const string langWhenEaten = $"{ModId}:when-eaten";
 
     public const string cuttingBoardInvClassName = $"{ModId}:cuttingBoard";
 
